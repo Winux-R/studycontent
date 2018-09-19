1093062fed6942379e8b42521df2b7b8e584d2ff
DESKTOP-6LJGE04\dell <jinxin@wisers.com>
2018-09-19 17:42:12 +0800

    第十一周

--- 1.课上笔记/珠峰大本营/11.正式课第十一周/day4/0915REDUX-全天讲义/src/index.js
+++ 1.课上笔记/珠峰大本营/11.正式课第十一周/day4/0915REDUX-全天讲义/src/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Vote from "./component/Vote";
-import 'bootstrap/dist/css/bootstrap.css';
+import React from './jsx渲染源原理复习/jsx_react';
+import ReactDOM from './jsx渲染源原理复习/jsx_reactdom';
 
 ReactDOM.render(<div>
-    <Vote title='王伟常丑不丑？'/>
+    <h2 className='title'>2018年第六期就业成绩</h2>
+    <ol className='clearfix'>
+        <li><em style={{color: 'red'}}>01</em><span>柯金珠同学入职腾讯，斩获￥20000月薪</span></li>
+        <li><em>02</em><span>任保森同学成功留级</span></li>
+    </ol>
+    GOOD GOOD STUDY!
 </div>, window.root);
-
